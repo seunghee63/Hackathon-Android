afa25a9ad624da3a13570aa207eccfe60e8c93f8
Sonjeongbeom <sjb990221@gmail.com>
2019-05-05 22:47:19 +0900

    make TodoFragment

---
+++ app/src/main/java/com/song2/sopt_24th_hackathon/Activity/DetailedTodoActivity.kt
@@ -0,0 +1,13 @@
+package com.song2.sopt_24th_hackathon.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.song2.sopt_24th_hackathon.R
+
+class DetailedTodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_todo)
+    }
+}
--- app/src/main/java/com/song2/sopt_24th_hackathon/Fragment/TodoFragment.kt
+++ app/src/main/java/com/song2/sopt_24th_hackathon/Fragment/TodoFragment.kt
@@ -3,22 +3,20 @@ package com.song2.sopt_24th_hackathon.Fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.song2.sopt_24th_hackathon.Adapter.TodoOverviewRecyclerViewAdapter
+import com.song2.sopt_24th_hackathon.Data.TodoOverviewData
 
 import com.song2.sopt_24th_hackathon.R
+import kotlinx.android.synthetic.main.fragment_todo.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TodoFragment : Fragment() {
+
+    lateinit var todoOverviewRecyclerViewAdapter: TodoOverviewRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +24,26 @@ class TodoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_todo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        var dataList: ArrayList<TodoOverviewData> = ArrayList()
+        dataList.add(TodoOverviewData(
+            "2019/05/05","받아쓰기 공부", "2포도알"))
+        dataList.add(TodoOverviewData(
+            "2019/05/05","받아쓰기 공부", "2포도알"))
+        dataList.add(TodoOverviewData(
+            "2019/05/05","받아쓰기 공부", "2포도알"))
+        dataList.add(TodoOverviewData(
+            "2019/05/05","받아쓰기 공부", "2포도알"))
+
+
+        todoOverviewRecyclerViewAdapter = TodoOverviewRecyclerViewAdapter(context!!, dataList)
+        rv_todo_overview.adapter = todoOverviewRecyclerViewAdapter
+        rv_todo_overview.layoutManager = GridLayoutManager(context!!, 2)
+
     }
 
 
