fc937c862bb56086b33b6984da8cfd694dc9c786
seunghee63 <0603yang@naver.com>
2019-05-06 04:46:37 +0900

    add FeedTab

--- app/src/main/java/com/song2/sopt_24th_hackathon/Fragment/FeedFragment.kt
+++ app/src/main/java/com/song2/sopt_24th_hackathon/Fragment/FeedFragment.kt
@@ -8,38 +8,61 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.song2.sopt_24th_hackathon.R
+import kotlinx.android.synthetic.main.fragment_feed.view.*
 
 class FeedFragment : Fragment() {
+
+    val habitFragment = HabitFragment()
+    val photoFragment = PhotoFragment()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_feed, container, false)
-        //addFragment()
+
+        addFragment(habitFragment)
+
+
+        view.rl_feed_fragment_photo_diary.setOnClickListener {
+            replaceFragment(photoFragment,0)
+        }
+
+        view.rl_feed_fragment_feed.setOnClickListener {
+            replaceFragment(habitFragment,1)
+        }
+
         return view
     }
 
-/*    fun addFragment(fragment : Fragment){
+    fun replaceFragment(fragment: Fragment, checkFlag : Int) {
         val fm = childFragmentManager
         val transaction = fm.beginTransaction()
 
-        *//*val myIntroFragment = MypageIntroFragment()
-        val bundle = Bundle()
-        bundle.putString("name", name)
-        bundle.putString("job", job)
-        bundle.putString("company", company)
-        bundle.putString("image", image)
-        bundle.putString("field", field)
-        bundle.putInt("userID", userID)
-        bundle.putInt("my_or_other_flag", my_or_other_flag)
-        Log.v("asdf", "보내는필드 = " + field)
-        bundle.putString("status", status)
-        bundle.putInt("coworkingEnabled", coworkingEnabled)
-        myIntroFragment.setArguments(bundle)*//*
+        if(checkFlag == 0){
+            val photoFragment = PhotoFragment()
 
-        transaction.add(R.id.ll_content_layout, myIntroFragment)
+            transaction.replace(R.id.ll_content_layout, photoFragment)
+            transaction.commit()
+        }
+        else{
+            val habitFragment = HabitFragment()
+
+            transaction.replace(R.id.ll_content_layout, habitFragment)
+            transaction.commit()
+        }
+
+
+    }
+
+    fun addFragment(fragment : Fragment){
+        val fm = childFragmentManager
+        val transaction = fm.beginTransaction()
+
+        transaction.add(R.id.ll_content_layout, fragment)
         transaction.commit()
-    }*/
+    }
 
 }
