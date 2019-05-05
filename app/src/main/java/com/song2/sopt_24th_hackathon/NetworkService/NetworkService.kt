61dfce895a0fb733a731720ab7856cfb594d12f7
seunghee63 <0603yang@naver.com>
2019-05-06 07:06:34 +0900

    modify gradle

--- app/src/main/java/com/song2/sopt_24th_hackathon/NetworkService/NetworkService.kt
+++ app/src/main/java/com/song2/sopt_24th_hackathon/NetworkService/NetworkService.kt
@@ -1,9 +1,7 @@
 package com.song2.sopt_24th_hackathon.NetworkService
 
 import com.song2.sopt_24th_hackathon.NetworkService.Get.GetTodoListResponse
-import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface NetworkService {
 
@@ -11,5 +9,5 @@ interface NetworkService {
 
     @GET("/toDo")
     fun getTodo(
-    ): Call<GetTodoListResponse>
+    ): retrofit2.Call<GetTodoListResponse>
 }
